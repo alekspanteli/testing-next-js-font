cf8ac6ad3c782a01234da7c6925cd4471c1188fb
alekspanteli <alekspanteli@rambler.ru>
2023-01-27 16:00:16 +0000

    update

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
 
     <div className="container px-4 mx-auto">
       <h1 className="text-3xl font-bold underline 2xl:text-orange-600">
-        Hello world!!!!!!!!!!!
+        Hello world!
       </h1>
       <p className="font-sans">In the example below, we use the font Inter from @next/font/google (You can use any font from Google or Local Fonts). Load your font with the variable option to define your CSS variable name and assign it to inter. Then, use inter.variable to add the CSS variable to your HTML document.</p>
       <p className="font-mono">In the example below, we use the font Inter from @next/font/google (You can use any font from Google or Local Fonts). Load your font with the variable option to define your CSS variable name and assign it to inter. Then, use inter.variable to add the CSS variable to your HTML document.</p>
